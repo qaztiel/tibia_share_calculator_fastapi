1de21036d75393eecfc750a6204c39248ebf4bde
qaztiel <19220186+qaztiel@users.noreply.github.com>
2022-11-11 06:42:55 +0000

    using codespaces on github

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
+
 from tibiasharecalculator import TibiaShareCalculator
 
 app = FastAPI()
 calc = TibiaShareCalculator()
 
+
+
+@app.get("/")
+async def root():
+    return {"message": "Tibia Share Calculator"}
+
+
 @app.get("/{level}")
 async def calc_level(level):
-    
+
     results = calc.calc_range(int(level))
-    
     return {
         "min": results['min_level'],
         "max": results['max_level']
