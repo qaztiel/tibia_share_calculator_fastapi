340942c842afd642ca6247ed0b640be0e2280ac4
josephaorahim <josephaorahim@yahoo.com>
2022-11-11 00:01:52 -0700

    using pytest

--- test_tibiasharecalculator.py
+++ test_tibiasharecalculator.py
@@ -1,16 +1,24 @@
-import unittest
+import pytest
+import math
 from tibiasharecalculator import TibiaShareCalculator
 
-class TestTibiaShareCalculator(unittest.TestCase):
+class TestTibiaShareCalculator:
 
     def test_min_level(self):
         nums_to_test = [-100, 0, 1, 100, 1000, 56, 89, 99, 1001]
         calc = TibiaShareCalculator()
         for num in nums_to_test:
-            self.assertEqual(calc.min_level(num), round(num*(2/3)))
+            assert (calc.min_level(num) == math.ceil(num*(2/3)))
+
 
     def test_max_level(self):
-        pass
+        nums_to_test = [-100, 0, 1, 100, 1000, 56, 89, 99, 1001]
+        calc = TibiaShareCalculator()
+        for num in nums_to_test:
+            assert (calc.max_level(num) == math.floor(num*(3/2)))
+
+
+
 
 if __name__ == '__main__':
-    unittest.main()
+    pass
